59d402c2ffadfeafc2b3c98e70cf9d541ceee063
Steffen Larson <steffman13@gmail.com>
2021-03-05 11:46:16 -0700

    Working on vaultpage

--- keepr-client/src/services/ProfilesService.js
+++ keepr-client/src/services/ProfilesService.js
@@ -1,14 +1,10 @@
 import { AppState } from '../AppState'
-import { logger } from '../utils/Logger'
 import { api } from './AxiosService'
 
 class ProfilesService {
   async getProfileById(id) {
-    // REVIEW this debugger never gets hit
-    // debugger
     const res = await api.get('api/profiles/' + id)
     AppState.activeProfile = res.data
-    logger.log('this is the profile', res.data)
   }
 }
 
